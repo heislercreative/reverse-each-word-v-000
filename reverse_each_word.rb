43019bc5e0d0d2e8a5fa482f0bbe08b52f992b53
Brennan Heisler <learn@heislercreative.com>
2018-01-24 02:39:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,12 @@
 #   reverse_array.join(" ")
 # end
 
-def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-    word.reverse
-  end
-  .join(" ")
-end
+# def reverse_each_word(sentence)
+#   sentence.split.collect do |word|
+#     word.reverse
+#   end
+#   .join(" ")
+# end
 
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
